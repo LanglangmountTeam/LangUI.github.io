93d1c11ddf789dd9822764e61420086f6afa4d65
尽意 <11469938+do-your-best@user.noreply.gitee.com>
2023-02-09 18:14:43 +0800

    完成了图标组件的基础开发

---
+++ packages/licon/doc/demo3.vue
@@ -0,0 +1,15 @@
+<template>
+    <licon size="30" color="green" del="1"><Camera/></licon>
+    <licon size="30" color="green" del="2"><Camera/></licon>
+    <licon size="30" color="green" del="3"><Camera/></licon>
+    <licon size="30" color="green" del="4"><Camera/></licon>
+    <licon size="30" color="green" del="5"><Camera/></licon>
+    <licon size="30" color="green" del="6"><Camera/></licon>
+    <licon size="30" color="green" del="7"><Camera/></licon>
+    <licon size="30" color="green" del="8"><Camera/></licon>
+    <licon size="30" color="green" del="9"><Camera/></licon>
+    <licon size="30" color="green" del="10"><Camera/></licon>
+</template>
+<script setup>
+import {Camera} from '@vicons/ionicons5';
+</script>
--- packages/licon/licon.vue
+++ packages/licon/licon.vue
@@ -1,14 +1,44 @@
 <template>
-	<div>
-		我是ikun
-	</div>
+	<Icon :color="color" :size="size" class="ico" :style="{opacity:`${del/10}`}"><slot></slot></Icon>
 </template>
 
 <script lang="ts">
 	export default{
 		name:"licon"
 	}
+	
 </script>
 
+<script lang="ts" setup>
+import { useAttrs,computed } from "vue";
+import { Icon } from '@vicons/utils';
+const props=defineProps({
+	color:{
+		color:String,
+		default:""
+	},
+	size:{
+		size:Number,
+		default:""
+	},
+	del:{
+		del:Number,
+		default:10
+	}
+})
+const color=computed(()=>{
+	return props.color
+});
+const size=computed(()=>{
+	return props.size?props.size:""
+});
+const del=computed(()=>{
+	return props.del?props.del:10
+});
+const attrs = useAttrs();
+</script>
 <style>
+.ico{
+	margin-right: 1vw;
+}
 </style>
